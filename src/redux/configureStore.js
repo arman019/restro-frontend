e1ca22e10eb907f0ce65d425e2b2413a9280a143
Arman <inzamam.arman@gmail.com>
2020-09-03 09:47:43 +0600

    splitting and combining reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,21 @@
 // we are gonna create store in here. create store has  attb reducer ,state, dispatcher
 
-import {createStore} from 'redux';
-import{ Reducer, initialState} from './reducer';
+import {createStore,combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore=() =>{
 
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes: Dishes,
+            comments:Comments,
+            promotions:Promotions,
+            leaders:Leaders
+        })
+
     );
     
     return store;
